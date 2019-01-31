241b6585a8f1c91bc524db7e36c1df6c8a8cf43a
s-gabor <ing.gabor@yahoo.com>
2019-01-31 13:36:04 +0200

    change code to models

--- views/games.js
+++ views/games.js
@@ -5,58 +5,89 @@ var container = document.getElementById("list-games");
 // onLoad display the games from API
 document.addEventListener("DOMContentLoaded", onHtmlLoaded);
 function onHtmlLoaded() {
-	$.get(apiGames)
-		.then(function(games) {
-			for (var i = 0; i < games.length; i++) {
-				// console.log(games[i]);
-				displayGame(games[i]);
-			}
-		})
-		.catch(function(err) {
-			alert('The following error occured: ', err);
-		})
+	var games = new Games();
+	games.getGames()
+			.then(function(response) {
+					console.log('OnHtmlLoaded received: ', response);
+					displayGames(response);
+				})
+			.catch(function(err) {
+					console.log('The following error occured: ', err);
+				})
+}
+
+
+function displayGames(games) {
+	for (var i = 0; i < games.length; i++) {
+		displayGame(games[i]);
+	}
 }
 
 
 // create an article with the following structure: title, description, image and 2 buttons(Edit and Delete)
 function displayGame(game) {
-	    var html = document.createElement("article");
-	    html.setAttribute("game-id", game._id);
-	    html.innerHTML = `<h2> ${game.title} </h2>
-	                      <p> ${game.description} </p>
-	                      <button class="btn-edit">Edit game</button>
-	                      <button class="btn-delete">Delete game</button>
-	                      <img src='${game.imageUrl}' width='500px' height='auto'>`;
-	    html.querySelector(".btn-edit").addEventListener("click", function () {
-	    	var btnEdit = this;
-	    	switchToEditMode(btnEdit);
-	    	editGame(html, btnEdit);
-	    });
-	    html.querySelector(".btn-delete").addEventListener("click", function() {
-	    	var userConfirmation = prompt('Are you sure you want to delete the game? To confirm type "yes"');
-	    	if (userConfirmation === 'yes') {
-	    		deleteGame(html);
-	    	} else {
-	    		alert('Action cancelled.');
-	    	}
-		});
-	    container.appendChild(html);
-}
+    var html = document.createElement("article");
+    html.setAttribute("game-id", game._id);
+    html.innerHTML = `<h2> ${game.title} </h2>
+                      <p> ${game.description} </p>
+                      <button class="btn-edit">Edit game</button>
+                      <button class="btn-delete">Delete game</button>
+                      <img src='${game.imageUrl}' width='500px' height='auto'>`;
+    html.querySelector(".btn-edit").addEventListener("click", editGame);
+    html.querySelector(".btn-delete").addEventListener("click", deleteGame);
+    container.appendChild(html);
 
+    // remove game
+    function deleteGame() {
+		var articleHtml = this.parentNode;
+		var id = articleHtml.getAttribute("data-id");
+		var gameObj = new Game({ _id: id });
+		gameObj.deleteGame()
+			.then(
+				function(response) {
+			        console.log("DELETE response", response);
+			        articleHtml.remove();
+		    	},
+				function(error) {
+			        alert("Cannot DELETE article");
+		    	}
+		    );
+	}
 
-// delete the game from API and refresh page
-function deleteGame(game) {
-	var id = game.getAttribute("game-id");
-	$.ajax({
-		url: apiGames + id,
-		method: "DELETE",
-		success: function(res) {
-			game.remove();
-			alert('The game was successfully deleted!');
+	// update game with user modifications(PUT to API)
+	function editGame() {
+		var article = this.parentNode;
+		var btn = this;
+		switchToEditMode(btn);
+		var titleField = article.children[0];
+		var descriptionField = article.children[1];
+		var imageUrlField = article.children[4];
+		var editableFields = [titleField, descriptionField];
+		for (var field of editableFields) {
+			field.setAttribute('contenteditable', true);
+			$(field).addClass('edit-mode');
 		}
-	});
+		btn.addEventListener('click', function() {
+			console.log('clicked on save changes!');
+			// "for loop" is NOT optional(apparently) because PUT doesn't refresh the page automatically(apparently)
+			for (var field of editableFields) {
+				field.setAttribute('contenteditable', false);
+				$(field).removeClass('edit-mode');
+			}
+			switchFromEditMode(btn);
+			var title = titleField.textContent;
+			var description = descriptionField.textContent;
+			var imageUrl = imageUrlField.getAttribute('src');
+			var id = article.getAttribute("game-id");
+			var game = new Game({_id: id});
+			game.updateGame({
+				title: title,
+				description: description,
+				imageUrl: imageUrl
+			});
+		})
+	}
 }
-
 
 // create an input article on "Add new game" click. Input from user: title, description and imageURL (id is created automatically by the API)
 document.getElementById('btn-add-game').addEventListener('click', function() {
@@ -76,54 +107,14 @@ $('#list-games').on('click', '.center button', function(event) {
 	var title = this.parentNode.children[0].value;
 	var description = this.parentNode.children[1].value;
 	var imageUrl = this.parentNode.children[2].value;
-	postGame({
+	var newGame = new Game({
 		title: title,
 		description: description,
 		imageUrl: imageUrl
 	});
+	newGame.postGame();
 })
 
-function postGame(dataObj) {
-	$.ajax({
-	  url: apiGames,
-	  method: 'POST',
-	  data: dataObj,
-	  success: function(response) {
-	    console.log(response);
-	  }
-	});
-}
-
-
-// update game with user modifications(PUT to API)
-function editGame(article, btn) {
-	var titleField = article.children[0];
-	var descriptionField = article.children[1];
-	var imageUrlField = article.children[4];
-	var editableFields = [titleField, descriptionField];
-	for (var field of editableFields) {
-		field.setAttribute('contenteditable', true);
-		$(field).addClass('edit-mode');
-	}
-	btn.addEventListener('click', function() {
-		console.log('clicked on save changes!');
-		// "for loop" is NOT optional(apparently) because PUT doesn't refresh the page automatically(apparently)
-		for (var field of editableFields) {
-			field.setAttribute('contenteditable', false);
-			$(field).removeClass('edit-mode');
-		}
-		switchFromEditMode(btn);
-		var title = titleField.textContent;
-		var description = descriptionField.textContent;
-		var imageUrl = imageUrlField.getAttribute('src');
-		var data = {
-					title: title,
-					description: description,
-					imageUrl: imageUrl
-					}
-		updateGame(article, data);
-	})
-}
 
 function switchToEditMode(btn) {
 	$(btn).addClass('btn-edit-mode');
@@ -135,14 +126,7 @@ function switchFromEditMode(btn) {
 	btn.textContent = 'Edit';
 }
 
-function updateGame(article, dataObj) {
-	var id = article.getAttribute("game-id");
-	$.ajax({
-		url: apiGames + id,
-		method: 'PUT',
-		data: dataObj,
-		success: function(response) {
-			console.log(response);
-		}
-	})
-}
+
+
+
+
